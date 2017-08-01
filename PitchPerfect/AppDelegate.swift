b548b92e08446a77fdc1fce42e149b1c24ea9cf7
Franz Noel Tanglao <franz@asteriainteractive.com>
2017-07-31 22:29:15 -0700

    feat: Changed console message on load

--- PitchPerfect/AppDelegate.swift
+++ PitchPerfect/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("hello","world!")
+        print("Loading PitchPerfect application!")
         return true
     }
 
